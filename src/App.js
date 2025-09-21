d6e03f5ba514a25eb1fd72a7a9df1d0274900747
Khan Adnan <adnanpathan9876@gmail.com>
2025-09-21 16:30:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Custom Hook Example - Product List</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
